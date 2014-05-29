84d493fcf59c70b8b22d3f39c24c48fc287b8365
Tom Uebel <tauebel@gmail.com>
2014-05-28 20:22:14 -0700

    Add core JSON parsing and email delivery functionality

--- email_service.rb
+++ email_service.rb
@@ -1,5 +1,18 @@
 require 'sinatra'
+require 'json'
+require 'mailgun'
 
 post '/' do
+  data = JSON.parse(request.body.read)
+  mailgun_client = Mailgun::Client.new ENV['MAILGUN_API_KEY']
 
+
+  message_params = {:from => 'noreply@example.com',
+                    :to => data["to"],
+                    :subject => data["subject"],
+                    :text => data["body"]
+
+  }
+
+  mailgun_client.send_message ENV['MAILGUN_TEST_DOMAIN'], message_params
 end
