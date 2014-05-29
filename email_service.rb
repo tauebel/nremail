fca36887720d4ff6acded702e7149570efcc6de1
Tom Uebel <tauebel@gmail.com>
2014-05-28 23:32:18 -0700

    Remove extra whitespace

--- email_service.rb
+++ email_service.rb
@@ -6,12 +6,10 @@ post '/' do
   data = JSON.parse(request.body.read)
   mailgun_client = Mailgun::Client.new ENV['MAILGUN_API_KEY']
 
-
   message_params = {:from => 'noreply@example.com',
                     :to => data["to"],
                     :subject => data["subject"],
                     :text => data["body"]
-
   }
 
   mailgun_client.send_message ENV['MAILGUN_TEST_DOMAIN'], message_params
